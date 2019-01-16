406cf83a80efde13a468480d3e6f01c1b1bbfe4c
Priscilla Pang <Priscilla.pang@keystonelaw.co.uk>
2019-01-16 00:00:30 +0000

    Checked and Validated

--- js/script.js
+++ js/script.js
@@ -2,55 +2,7 @@
 Treehouse Techdegree:
 FSJS project 1 - A Random Quote Generator
 ******************************************/
-
-// Study guide for this project - https://drive.google.com/file/d/1s5grutGuQFwJcQP8bFwEI69Q8FCkGdDk/view?usp=sharing
-
-
-/***
-  Create the array of quote objects and name it `quotes`. - done
-  Add at least five quote objects to the `quotes` array. - done
-  Give each quote object a `quote` and `source` property. - done
-
-  Recommended:
-    - Add at least one `year` and/or `citation` property to at least one
-      quote object. - done
-***/
-
-
-
-
-/***
-  Create the `getRandomQuote` function to:
-   - generate a random number - done
-   - use the random number to `return` a random quote object from the
-     `quotes` array. - done
-***/
-
-
-
-
-/***
-  Create the `printQuote` function to:
-   - call the `getRandomQuote` function and assign it to a variable. - done
-   - use the properties of the quote object stored in the variable to
-     create your HTML string. - done
-   - use conditionals to make sure the optional properties exist before
-     they are added to the HTML string.
-   - set the `innerHTML` of the `quote-box` div to the HTML string. - done
-***/
-
-
-
-
-/***
-  When the "Show another quote" button is clicked, the event listener
-  below will be triggered, and it will call, or "invoke", the `printQuote`
-  function. So do not make any changes to the line of code below this
-  comment.
-***/
-
 //Variables
-const body = document.getElementsByTagName("body")[0];
 const quotes = [{"quote": "I think, therefore I am",
                "source":"Rene Descarte",
                "citation":  "Discourse on the method",
@@ -85,23 +37,21 @@ let citation = document.getElementsByClassName("citation")[0];
 //Functions
 
 function getRandomQuote() {
-  //loop through array to stick generated number with the array item
+  //loop through array to "stick" generated number with the array item
   let randomNum = Math.floor(Math.random() * quotes.length);
   let randomQuote = quotes[randomNum];
   return randomQuote;
 
-};//end of function
+}//end of function
 
 
 function printQuote() {
   let rawQuote = getRandomQuote();
-  console.log(rawQuote);
   //access properties
   const newQuote = rawQuote.quote;
   const newSource = rawQuote.source;
   const newCitation = rawQuote.citation;
   const newCategory = rawQuote.category;
-  console.log(newCategory);
 
   //replace innerhtml
   quote.innerHTML = newQuote;
@@ -131,21 +81,11 @@ function printQuote() {
   return source;
   return citation;
 
-}; //end of function
+} //end of function
 
 //Event Handler
 //Click Handler
 document.getElementById('loadQuote').addEventListener("click", printQuote);
 
 //Timed Handler
-setInterval(printQuote,3000);
-
-
-
-
-
-
-// For example, a tags property could include a list of "tags" like "humor", "business", or "politics" to categorize each quote.
-//Use the new properties you've created to conditionally add to the string that gets printed to the screen.
-
-// Remember to delete the comments that came with this file, and replace them with your own code comments.
+setInterval(printQuote,3010);
